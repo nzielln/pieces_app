11b830dfbbbdb846309d8e915c280a415a3197fe
Ellah Nzikoba <ellahnzikoba@gmail.com>
2022-01-24 12:45:08 -0500

    Initial Commit

---
+++ Pieces/PiecesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiecesApp.swift
+//  Pieces
+//
+//  Created by Ellah Nzikoba on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct PiecesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
